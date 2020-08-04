99b9d14bb48aba048e501cec5b6b5f53a8273585
ShashwatMDas <shashwatmdas@gmail.com>
2020-08-04 20:01:18 +0530

    Production build

---
+++ build/precache-manifest.89d3842263eb63f913378805fb108037.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b25b5126cb588154886347ecd7b67f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e60c7b2196b2f2f2b955",
+    "url": "/static/css/2.f085cf01.chunk.css"
+  },
+  {
+    "revision": "891e45724bc6b1ae1f4c",
+    "url": "/static/css/main.c2a17bc5.chunk.css"
+  },
+  {
+    "revision": "e60c7b2196b2f2f2b955",
+    "url": "/static/js/2.68ff3b2c.chunk.js"
+  },
+  {
+    "revision": "30360dfd65dbd4cc0560bb7e38519ae1",
+    "url": "/static/js/2.68ff3b2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "891e45724bc6b1ae1f4c",
+    "url": "/static/js/main.d88b529b.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
