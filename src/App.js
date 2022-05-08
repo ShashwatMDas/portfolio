c124e4567cd4613a084153c1d594ebfc501dd704
ShashwatMDas <shashwatmdas@gmail.com>
2022-05-09 05:25:13 +0530

    Changed favicon and added preloader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Home from './screens/Home'
 import About from './screens/About'
@@ -11,10 +11,43 @@ import {
 } from "react-router-dom";
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
+import Lottie from 'react-lottie';
+import animationData from './loading.json';
 const starsCount = window.screen.width > 600 ? 35 : 10;
 
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoading = () => {
+    setIsLoading(false);
+  }
+
+  const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+      }
+  };
+
+  useEffect(()=>{
+    window.addEventListener("load",handleLoading);
+    return () => window.removeEventListener("load",handleLoading);
+  },[])
+  if(isLoading) {
+    return (
+      <div style={{height: '100vh', width: '100vw', backgroundColor: "#000", justifyContent: 'center', alignItems: 'center', paddingTop: '20vh'}}>
+        <Lottie 
+          options={defaultOptions}
+          height={400}
+          width={400}
+          style={window.screen.width > 600 ? {} : {marginLeft: '-4vw'}}
+        />
+      </div>
+    )
+  }
   return (
     <ThemeProvider theme={THEME}>
     <div className="App">
